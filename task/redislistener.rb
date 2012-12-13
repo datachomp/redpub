0516a5e45434732cdc5c1cdf0bd19714f5c85bde
datachomp <rob@datachomp.com>
2012-12-13 14:34:03 -0600

    added basic pubsub commands

---
+++ task/redislistener.rb
@@ -0,0 +1,18 @@
+cli 1
+subscribe customers
+
+
+cli2
+
+publish customers "robs@example.com"
+publish customers "robc@example.com"
+
+
+
+or
+
+subscribe customers alerts
+
+
+publish customers "robs@example.com"
+publish alerts ""
