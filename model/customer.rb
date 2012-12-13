11fca92e43f97e695d22d808e546cf21b300e6d6
rob sullivan <rob@datachomp.com>
2012-12-13 00:43:45 -0600

    wrapping customer actions in a transaction

--- model/customer.rb
+++ model/customer.rb
@@ -4,37 +4,16 @@ class Customer
 	$redis = Redis.new
 
     def self.createcustomer(email, name, happy)
+      
+      #All or nothing here. Everything will execute in order
+      # as well as stay isolated, and rollback  
+      $redis.multi()
         keyid = $redis.incr "customerid"
         customerkey = 'customer:' + keyid.to_s
      
         $redis.hmset customerkey, 'email',email, 'name', name, 'happy', happy, 'signupdate', Time.now
         $redis.lpush('customerlist', customerkey)
         $redis.lpush('customeremaillist', email)
+      $redis.exec()
     end
-
-#	def seedcustomer
-#		redis.set "customerid",0
-        
-#        customer = Hash.new
-        
-#        customer = {"customerid"=> getpk,
-#               "email"=>'robs@example.com', "name"=>'rob sullivan', "happy"=>'yes', "signupdate"=>Time.now }
-
-        #redis.hmset customer["customerid"], customer
-#        redis.lpush('customerlist', customer["customerid"])
-#        redis.lpush('customeremaillist', customer["email"])
-            
-#        customerid = redis.incr "customerid"                    
-#        customer = {"customerid"=> 'customer:' + customerid,
-#                    "email"=>'robc@example.com', "name"=>'rob conery', "happy"=>'yes' }
-#        redis.hmset invoicekey, 'email',email, 'video', video, 'dateordered', dateordered
-#        redis.lpush('invoicelist', invoicekey)
-#	end
-	
-#	def getpk
-#		customerid = redis.incr "customerid"
-#		"customer:#{customerid}"
-#	end
-
-
 end
